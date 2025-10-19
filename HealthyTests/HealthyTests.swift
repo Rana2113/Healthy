7faa4e08273ed8b2089dd65d71e38fa07c3b54ed
Abdelrahman Zain <boody.ali.zean@gmail.com>
2025-10-20 01:07:19 +0300

    feat(ui): add UIButton style extension

---
+++ HealthyTests/HealthyTests.swift
@@ -0,0 +1,17 @@
+//
+//  HealthyTests.swift
+//  HealthyTests
+//
+//  Created by Rana Mustafa on 15/10/2025.
+//
+
+import Testing
+@testable import Healthy
+
+struct HealthyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
