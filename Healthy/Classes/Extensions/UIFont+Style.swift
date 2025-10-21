830905eb5432409952d70a5ad84e9720d35028d8
Rana Mustafa Hassan Qutb <rna0882@gmail.com>
2025-10-21 13:56:42 +0300

    feat: add Poppins fonts to Info.plist and UIFont+Style extension

---
+++ Healthy/Classes/Extensions/UIFont+Style.swift
@@ -0,0 +1,73 @@
+import UIKit
+
+// MARK: - Regular Fonts
+
+extension UIFont {
+    static var titleRegular: UIFont {
+        poppins(ofSize: 50, weight: .regular)
+    }
+    static var headerRegular: UIFont {
+        poppins(ofSize: 30, weight: .regular)
+    }
+    static var largeRegular: UIFont {
+        poppins(ofSize: 20, weight: .regular)
+    }
+    static var mediumRegular: UIFont {
+        poppins(ofSize: 18, weight: .regular)
+    }
+    static var normalRegular: UIFont {
+        poppins(ofSize: 16, weight: .regular)
+    }
+    static var smallRegular: UIFont {
+        poppins(ofSize: 14, weight: .regular)
+    }
+}
+
+// MARK: - Regular Fonts
+
+extension UIFont {
+    static var titleBold: UIFont {
+        poppins(ofSize: 50, weight: .bold)
+    }
+    static var headerBold: UIFont {
+        poppins(ofSize: 30, weight: .bold)
+    }
+    static var largeBold: UIFont {
+        poppins(ofSize: 20, weight: .bold)
+    }
+    static var mediumBold: UIFont {
+        poppins(ofSize: 18, weight: .bold)
+    }
+    static var normalBold: UIFont {
+        poppins(ofSize: 16, weight: .bold)
+    }
+    
+    static var smallBold: UIFont {
+        poppins(ofSize: 14, weight: .bold)
+    }
+    
+    static var smallerBold: UIFont {
+        poppins(ofSize: 12, weight: .bold)
+    }
+}
+
+// MARK: Poppins UIFont Helpers
+
+private extension UIFont {
+    static func poppins(ofSize size: CGFloat, weight: UIFont.Weight) -> UIFont {
+        let fontName = poppinsFontName(forWeight: weight)
+        guard let font = UIFont(name: fontName, size: size) else {
+            assertionFailure("Unable to get a font: \(fontName)")
+            return UIFont.systemFont(ofSize: size, weight: weight)
+        }
+        return font
+    }
+    static func poppinsFontName(forWeight weight: UIFont.Weight) -> String {
+        switch weight {
+        case .bold:
+            return "Poppins-Bold"
+        default:
+            return "Poppins-Regular"
+        }
+    }
+}
