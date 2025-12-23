6b4d6816d8f6b0b52ab177246efb7d8ad7080bcd
Rana Mustafa Hassan Qutb <rna0882@gmail.com>
2025-12-23 19:16:03 +0200

    feat: create sign-in view layout and social login UI
    - Added sign-in screen layout using XIB
    - Integrated social media buttons UI
    - Applied FormTextField for email and password

---
+++ Healthy/Classes/Modules/Onboarding/Login/LoginViewModelType.swift
@@ -0,0 +1,13 @@
+import Foundation
+
+/// LoginViewModelType Input & Output
+///
+typealias LoginViewModelType = LoginViewModelInput & LoginViewModelOutput
+
+/// LoginViewModelType ViewModel Input
+///
+protocol LoginViewModelInput {}
+
+/// LoginViewModelType ViewModel Output
+///
+protocol LoginViewModelOutput {}
