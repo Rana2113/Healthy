8b96f6b331ea97f5a8c6a5982e97c3ccd3233be2
Rana Mustafa <146140834+Rana2113@users.noreply.github.com>
2026-02-06 02:50:01 +0200

    Resolve develop conflicts + unit tests for auth flow (HL-38, HL-39, HL-40) (#29)
    * feat(logger): add full logging system with FileSystem & NewRelic integration (#14)
    
    * feat(splash) : [HLTH-3] Add Splash Screen & App/Onboarding Coordinators (#12)
    
    * SplashScreen Design
    
    * SplashScreen Design
    
    * Remove local Xcode user state file
    
    * chore(xcode): update project settings with team ID and disable user script sandboxing
    
    * SplashScreen Design
    
    ---------
    
    Co-authored-by: Mohamed Khaled <muhameddkhalid@gmail.com>
    
    * chore(logger): add New Relic run script and logger files
    
    * chore(logger): add full logging system with FileSystemLogger and NewRelicLogger
    
    ---------
    
    Co-authored-by: Mohamed Khaled <muhameddkhalid@gmail.com>
    
    * feat: [HL-45] main tab bar controller, [HL-44] Validator Unit Testing (#28)
    
    * Add main tab bar controller
    
    * feat : Add validators unit test
    
    * test: [HL-38] Create account view model <> Unit Testing
    
    * test: [HL-38] Create account view model <> Unit Testing
    
    * test: [HL-40] Sign-in view controller <> Unit Testing
    
    * test: [HL-39] Sign-in page logic <> Unit Testing
    
    ---------
    
    Co-authored-by: Abdelrahman Zain <116590961+AbdelrahmanZAlN@users.noreply.github.com>
    Co-authored-by: Mohamed Khaled <muhameddkhalid@gmail.com>
    Co-authored-by: Abdelrahman Zain <boody.ali.zean@gmail.com>

--- Healthy/Classes/AppDelegate.swift
+++ Healthy/Classes/AppDelegate.swift
@@ -1,5 +1,4 @@
 import UIKit
-import NewRelic
 import GoogleSignIn
 import NewRelic
 
---
+++ HealthyTests/Classes/Modules/Onboarding/Login/Mocks/LoginViewModelMock.swift
@@ -0,0 +1,56 @@
+import Combine
+@testable import Healthy
+
+final class LoginViewModelMock: LoginViewModelType {
+    
+    private let errorSubject = PassthroughSubject<Error, Never>()
+    
+    var errorPublisher: AnyPublisher<Error, Never> {
+        errorSubject.eraseToAnyPublisher()
+    }
+    
+    var isLoadingIndicatorPublisher: AnyPublisher <Bool, Never>{
+        Just(false).eraseToAnyPublisher()
+    }
+    
+    var isLoginEnabledPublisher: AnyPublisher<Bool,Never> {
+        Just(false).eraseToAnyPublisher()
+    }
+    var isLoginStatusPublisher: AnyPublisher<Bool,Never> {
+        Just(false).eraseToAnyPublisher()
+    }
+
+    func updateEmail(_ text: String) {}
+    
+    func updatePassword(_ text: String) {}
+    
+    func onLoadingIndicator(state: @escaping (Bool) -> Void) {}
+    
+    func onErrorMessage(message: @escaping (String) -> Void) {}
+    
+    func onButtonEnabled(onEnabled: @escaping (Bool) -> Void) {}
+    
+    func onLoginStatus (onEnabled: @escaping (Bool) -> Void) {}
+    
+    private(set) var performSignInCallCount: Int = .zero
+    func performSignIn() {
+        performSignInCallCount += 1
+    }
+
+    
+    private(set) var performSignUpCallCount: Int = .zero
+    func performSignUp() {
+        performSignUpCallCount += 1
+    }
+
+    private(set) var performForgetPasswordCount: Int = .zero
+    func performForgetPassword() {
+        performForgetPasswordCount += 1
+    }
+
+    private(set) var performSocialMediaSignInCount: Int = .zero
+    func performSocialMediaSignIn(_ authentication: Healthy.Authentication) {
+        performSocialMediaSignInCount += 1
+    }
+
+}
